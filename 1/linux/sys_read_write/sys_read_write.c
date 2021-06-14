8ded85c73bfbd4dbeed55a4021ca2ecd4b1b584f
APF2000 <arthurpfonseca@gmail.com>
2021-06-14 19:00:42 -0300

    Adicionando Petterson

--- 1/linux/sys_read_write/sys_read_write.c
+++ 1/linux/sys_read_write/sys_read_write.c
@@ -3,6 +3,33 @@
 
 long values[5] = { 0, 1, 2, 3, 4 };
 
+int interested[2] = { 0, 0 };
+int turn;
+
+void enter_region(int process)
+{
+	pr_info("comeco: interested = { %d, %d } \n", interested[0], interested[1]);
+
+	int other = 1 - process;
+	interested[process] = 1;
+	turn = process;
+
+	pr_info("Proc %d esperando\n", process);
+	pr_info("meio: turn = %d; interested = { %d, %d } \n", turn, interested[0], interested[1]);
+	while(turn == process && interested[other] == 1){ 
+		pr_info("fim: proc %d; interested = { %d, %d } \n", interested[0], interested[1]);
+	}
+
+	pr_info("Proc %d ta on\n", process);
+}
+
+void leave_region(int process)
+{
+	pr_info("Proc %d vai ficar off\n", process);
+	interested[process] = 0;
+	pr_info("Proc %d ta off\n", process);
+}
+
 asmlinkage long sys_read_number(long which)
 {
 	pr_info("Reading index %ld\n", which);
