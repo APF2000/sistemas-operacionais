acb32f1e79c8da071b42ada68b648dc6b13cf24d
APF2000 <arthurpfonseca@gmail.com>
2021-06-17 21:50:54 -0300

    Dando kernel panic

--- 1/initramfs/init.c
+++ 1/initramfs/init.c
@@ -27,34 +27,46 @@ int change_id(long status)
 }
 
 // num, turn, interested[0], interested[1]
-void enter_region(long process)
+void enter_region(long process, int **vals)
 {
 	int other = 1 - process;
-	write_number(2+process,TRUE);  //interested[process] = TRUE;
-	write_number(1,process);	//turn = process;
 	
+	//write_number(2+process,TRUE);  //interested[process] = TRUE;
+	//write_number(1,process);	//turn = process;
+	
+	(*vals)[2 + process] = TRUE;
+	(*vals)[1] = process;
 
 	//printf("[%ld] INICIO ENTER REGION. turn=[%ld] interested = { %d, %d } \n", process, read_number(1), read_number(2), read_number(3));
 
 	// while(turn == process && interested[other] == TRUE)
-	while(read_number(1) == process && read_number(3-process) == TRUE);
+	while((*vals)[1] == process && (*vals)[3-process] == TRUE);
 
-	printf("[%d] ENTER REGION. turn=[%ld] interested = { %d, %d } \n", process, read_number(1), read_number(2), read_number(3));
+	printf("[%d] ENTER REGION. turn=[%ld] interested = { %d, %d } \n", process, (*vals)[1], (*vals)[2], (*vals)[3]);
 }
 
 // num, turn, interested[0], interested[1]
-void leave_region(long process)
+void leave_region(long process, int **vals)
 {
-	write_number(2+process,FALSE);  //interested[process] = FALSE;
+	//write_number(2+process,FALSE);  //interested[process] = FALSE;
+	(*vals)[2 + process] = FALSE;	
+	
 	printf("[%d] LEFT REGION.\n", process);
 }
 
-int *foo(int status)
+typedef struct aux{
+	int status;
+	int **vals;
+};
+
+int *foo(struct aux input)
 {	
 	int i;
+	int status = input.status;
+	int **vals = input.vals;
 
 	while(1){
-		enter_region(change_id(status));
+		enter_region(change_id(status), vals);
 		x = read_number(0);
 		printf("[%d] Read number: %ld.\n", status, x);
 
@@ -65,24 +77,37 @@ int *foo(int status)
 		printf("[%d] Going to write: %ld.\n", status, x);
 		write_number(0, x);
 		printf("[%d] Wrote: %ld.\n", status, x);
-		leave_region(change_id(status));
+		leave_region(change_id(status), vals);
 	}
 	return NULL;
 }
 
 int main()
 {
+	int *values;//[4] = {0, 0, 0, 0}; // num, turn, interested[0], interested[1]
+	values = malloc(4 * sizeof(int));
+	values[0] = 0;
+	values[1] = 0;
+	values[2] = 0;
+	values[3] = 0;
 
 	pthread_t th0, th1;
 	int *r_th0, *r_th1;
 
+	struct aux *a1, *a2;
+	a1->status = 0;
+	a1->vals = &values;
+
+	a2->status = 1;
+	a2->vals = &values;
+
 	printf("Thread Main: Algoritmo de Peterson.");
-	if( pthread_create( &th0, NULL, (void*)foo, 0 ) != 0 ){
+	if( pthread_create( &th0, NULL, (void*)foo, (void*)a1 ) != 0 ){
 		printf("Error pthread_create p/ Thread 0.");
 		exit(1);
 	}
 
-	if( pthread_create( &th1, NULL, (void*)foo, 1 ) != 0 ){
+	if( pthread_create( &th1, NULL, (void*)foo, (void*)a2 ) != 0 ){
 		printf("Error pthread_create p/ Thread 1.");
 		exit(1);
 	}
