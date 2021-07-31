25ddc969a568192a511deccce08e7d1c2a23e872
APF2000 <arthurpfonseca@gmail.com>
2021-07-31 12:41:54 -0300

    Esse aqui e o lugar certo pra por o large.c

---
+++ initramfs_base/src/large.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_MEM 32 * 1024 * 1024// 64MB de memoria total, palavra tem 4 bytes (divide por 4) 
+
+static int *vector;//[MAX_MEM / ( 3 * sizeof(int) ];
+
+int main()
+{
+	long long size = MAX_MEM / 3;//20000000;//MAX_MEM/12;//2062500;//MAX_MEM / ( 3 * sizeof(int) );//[size];// = { 12345 }; // vetor pra lotar memoria
+
+	vector = malloc(size * sizeof(int));
+
+	printf("ponteiro para o vetor: %lld\n", (long long)vector);
+
+	while(1)
+	{
+		for(long long i = 0; i < size; i++)
+		{
+			vector[i] = 12345;
+		}
+	}
+
+	return 0;
+}
